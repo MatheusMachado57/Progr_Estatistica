37dffbade113fe910d2c36bd4919b80ff6fced00
Matheus Machado de Almeida <40668494+MatheusMachado57@users.noreply.github.com>
2021-06-22 13:24:57 -0300

    Update Semana 4.R

--- Semana 4.R
+++ Semana 4.R
@@ -107,7 +107,10 @@ Pos_Minimo = function(v){
 Pos_Minimo(c(10,0,-2,8,90))
 Pos_Minimo(c(10,0,-2,8,90,-2))
 
-# Q3
+# Q3 - FaÇa o que se pede sem usar as funções mean e sum do R. No computador implemente
+# o algoritmo visto em sala de aula que recebe como entrada um vetor v e retorna a
+# média amostral dos valores em v.
+# OBS: Se quiser use a função mean para comparação.
 Media = function(v){
   if(class(v)!="numeric") stop("Entre com um vetor de números.")
   n     = length(v)
@@ -119,7 +122,10 @@ Media = function(v){
 Media(c(10,0,-2,8,90))
 mean(c(10,0,-2,8,90))
 
-# Q4
+# Q4 - Faça o que se pede sem usar a função median do R. Para ordenar o vetor de entrada
+# use a função sort do R. No computador implemente o algoritmo visto em sala de
+# aula que recebe como entrada um vetor v e retorna a mediana de v.
+# OBS: Se quiser use a função median para comparação.
 Mediana = function(v){
   if(class(v)!="numeric") stop("Entre com um vetor de números.")
   n     = length(v)
@@ -133,8 +139,9 @@ median(c(10,0,-2,8,90))
 Mediana(c(10,0,-2,8,90,6))
 median(c(10,0,-2,8,90,6))
 
-# Q5
-# a
+# Q5 - Faça o que se pede sem usar a função quantile do R. Para ordenar o vetor de entrada use a função sort do R.
+
+# a - No computador implemente o pseudo-código visto em sala de aula, baseado no Método 1, que recebe como entrada um vetor v e retorna os três quartis.
 Quartis_1 = function(v){
   if(class(v)!="numeric") stop("Entre com um vetor de números.")
   n     = length(v)
@@ -155,7 +162,8 @@ Quartis_1 = function(v){
 }
 Quartis_1(c(10,0,-2,8,90))
 
-# b
+# b - No caderno escreva um pseudo-código que calcula os três quartis a partir do Método 2.
+
 # Entrada: v = vetor com os numeros
 # Saida: quartis de v
 # 1 Defina n = tamanho do vetor v
@@ -169,7 +177,7 @@ Quartis_1(c(10,0,-2,8,90))
 # 9 Defina q3 = mediana de v2
 # 10 Retorne o vetor (q1, q2, q3)
 
-# c
+# c - No computador implemente o pseudo-código escrito no item acima.
 Quartis_2 = function(v){
   if(class(v)!="numeric") stop("Entre com um vetor de números.")
   n     = length(v)
@@ -192,8 +200,11 @@ Quartis_1(c(10,0,-2,8,90))
 Quartis_2(c(10,0,-2,8,90))
 quantile(c(10,0,-2,8,90),c(0.25,0.5,0.75))
 
-# Q6
-# a
+# Q6 - Fa¸ca o que se pede sem usar a função table ou algo parecido.
+
+# a - No computador implemente o algoritmo visto em sala de aula que recebe como
+# entrada um vetor v e retorna uma matriz cuja primeira linha guarda os diferentes
+# valores de v e a segunda as frequências absolutas com que esses valores aparecem no vetor de dados.
 FreqsAbs = function(v){
   if(class(v)!="numeric") stop("Entre com um vetor de números.")
   n       = length(v)
@@ -225,7 +236,8 @@ table(c(10,0,-2,8,90))
 FreqsAbs(c(10,0,-2,8,90,90,90,0))
 FreqsAbs(c(10,0,8,90,90,90,0))
 
-# b
+# b - Repita o item 6a gerando agora as frequências relativas. 
+# Não use o mesmo nome da função implementada no item 6a.
 FreqsRel = function(v){
   if(class(v)!="numeric") stop("Entre com um vetor de números.")
   n       = length(v)
@@ -257,7 +269,8 @@ FreqsRel(c(10,0,-2,8,90))
 FreqsRel(c(10,0,-2,8,90,90,90,0))
 FreqsRel(c(10,0,8,90,90,90,0))
 
-# c
+# c - Faça agora uma outra função que recebe como argumento além do vetor v um objeto to tipo "logic", vamos chamá-lo de rel. 
+# Se rel = T RUE a matriz de saída deve guardar as frequências relativas, caso contrário ela guarda a frequência absoluta.
 Freq = function(n, rel){
   if(class(rel)!="logical") stop("O segundo argumento desta funcao deve ser um objeto do tipo logico onde TRUE retorna as freq relativas e FALSE retorna as freq absolutas")
   if(rel==TRUE){
@@ -270,7 +283,7 @@ Freq(c(10,0,8,90,90,90,0),TRUE)
 Freq(c(10,0,8,90,90,90,0),FALSE)
 Freq(c(10,0,8,90,90,90,0),"A")
 
-# Q7
+# Q7 - Usando as funções implementadas em 6, implemente o algoritmo visto em sala de aula que recebe como entrada um vetor de dados e retorna a moda desse vetor.
 Moda = function(v){
   M = FreqsAbs(v)
   j = Pos_Maximo(M[2,])
@@ -283,15 +296,18 @@ u = c(10,0,8,90,90,0)
 FreqsAbs(u)
 Moda(u)
 
-# Q8
-# a
+# Q8 
+
+# a - Primeiro escreva no caderno um pseudo-código para o algoritmo que recebe como entrada um vetor v e retorna a sua Amplitude Total. 
+# Não é para usar o computador ainda. Considere que você já sabe calcular os valores mínimo e máximo do seu vetor de entrada.
+
 # Entrada: v = vetor com os numeros
 # Saida: amplitude total de v
 # 1 Defina min = valor minimo do vetor v
 # 2 Defina max = valor maximo do vetor v
 # 3 Retorne at = max - min
 
-# b
+# b - Agora no computador implemente o pseudo-código feito do item 8a usando as funções implementadas nos exercícios 1 e 2.
 AT = function(v){
   min = Minimo(v)
   max = Maximo(v)
@@ -300,14 +316,17 @@ AT = function(v){
 AT(c(1,0,0,0,20,3))
 
 # Q9
-# a
+# a - Primeiro escreva no caderno um pseudo-código para o algoritmo que recebe como
+# entrada um vetor v e retorna a sua Distância Interquartílica. Não é para usar o
+# computador ainda. Considere que você já sabe calcular os quartis de v.
+
 # Entrada: v = vetor com os numeros
 # Saida: distancia interquartilica de v
 # 1 Defina q1 = primeiro quartil do vetor v
 # 2 Defina q3 = terceiro quartil do vetor v
 # 3 Retorne di = q3 - q1
 
-# b
+# b - Agora no computador implemente o passo-a-passo feito do item 9a usando as funções implementadas no exercício 5.
 DI = function(v){
   qs  = Quartis_2(v)
   q1 = qs[1]
@@ -316,7 +335,10 @@ DI = function(v){
 }
 DI(c(1,0,0,0,20,3))
 
-# Q10
+# Q10 - Faça o que se pede sem usar as funções sd, mean e sum do R. 
+# No computador implemente o algoritmo visto em sala de aula que recebe como entrada um vetor v e
+# retorna a sua variância amostral. Para simplificar use a função implementada no exercício 3.
+# OBS: Se quiser use a função sd para comparação.
 VarAm = function(v){
   n = length(v)
   m = Media(v)
@@ -331,7 +353,10 @@ VarAm(c(1,0,0,0,20,3))
 var(c(1,0,0,0,20,3))
 
 # Q11
-# a 
+# a - Primeiro escreva no caderno um pseudo-código para o algoritmo que recebe como
+# entrada um vetor v e retorna o seu Desvio Médio. Não é para usar o computador
+# ainda. Considere que você já sabe calcular a média de v.
+
 # Entrada: v = vetor com os numeros
 # Saida: desvio medio dos valores de v
 # 1 Defina n = tamanho do vetor v
@@ -344,7 +369,7 @@ var(c(1,0,0,0,20,3))
 # 8 Faca s2 = soma / n
 # 9 Retorne s2
 
-# b
+# b - Agora no computador implemente o pseudo-código feito do item 11a.
 DesAm = function(v){
   n = length(v)
   m = Media(v)
@@ -361,8 +386,10 @@ library(lsr)
 aad(x)
 aad
 
-# Q12
-# a
+# Q12 - Fa¸ca o que se pede sem usar as funções mean e sum do R.
+# a - Implemente o algoritmo visto em sala de aula que recebe como entrada dois
+# vetores v e w e retorna a covariância amostral entre eles. Para simplificar use a
+# função implementada no exercício 3.
 CovAm = function(v,w){
   if( (class(v)!="numeric") || (class(w)!="numeric") ) stop("Os 2 argumentos precisam ser vetores numericos.")
   n = length(v)
@@ -386,7 +413,14 @@ w = c(5,0,1,5)
 CovAm(v,w)
 cov(v,w)
 
-# b
+# b - Desafio: vamos generalizar o item 12a. Implemente uma função que recebe como
+# entrada uma matriz de dados. Considere que cada coluna dessa matriz representa
+# um vetor de dados. A função implementada deve retornar a matriz de covariância
+# amostral entre os vetores coluna dessa matriz.
+
+# Lembre-se que a matriz de covariância é definida pela matriz cuja posição (i, j) guarda a
+# covariância da variável i com a j.
+
 # Entrada: M = matriz com os dados sendo que cada coluna representa uma variavel
 # Saida: matriz de covariancias amostrais
 # 1 Defina k = numero de variaveis
